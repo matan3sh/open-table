2d6ee29baefdc6574f7853ecf06ec70e34a4373c
Matan Shaviro <matan3sh@gmail.com>
2023-03-10 11:44:23 +0200

    Improve file structure

--- components/search/SearchRestaurantCard.tsx
+++ components/search/SearchRestaurantCard.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function SearchRestaurantCard() {
   return (
     <div className="border-b flex pb-5">
@@ -20,7 +22,7 @@ export default function SearchRestaurantCard() {
           </div>
         </div>
         <div className="text-red-600">
-          <a href="">View more information</a>
+          <Link href="/restaurant/milestones-grill">View more information</Link>
         </div>
       </div>
     </div>
--- pages/restaurant/[slug]/index.tsx
+++ pages/restaurant/[slug]/index.tsx
@@ -1,9 +1,12 @@
 import Navbar from "../../../components/Navbar";
+import RestaurantDescription from "../../../components/restaurant/RestaurantDescription";
 import RestaurantHeader from "../../../components/restaurant/RestaurantHeader";
 import RestaurantImages from "../../../components/restaurant/RestaurantImages";
+import RestaurantRating from "../../../components/restaurant/RestaurantRating";
 import RestaurantReservation from "../../../components/restaurant/RestaurantReservation";
 import RestaurantReviews from "../../../components/restaurant/RestaurantReviews";
 import RestaurantTabs from "../../../components/restaurant/RestaurantTabs";
+import RestaurantTitle from "../../../components/restaurant/RestaurantTitle";
 
 export default function RestaurantDetails() {
   return (
@@ -11,38 +14,12 @@ export default function RestaurantDetails() {
       <main className="max-w-screen-2xl m-auto bg-white">
         <Navbar />
         <RestaurantHeader />
-        {/* DESCRIPTION PORTION */}
         <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
           <div className="bg-white w-[70%] rounded p-3 shadow">
             <RestaurantTabs />
-            {/* TITLE */}
-            <div className="mt-4 border-b pb-6">
-              <h1 className="font-bold text-6xl">Milesstone Grill</h1>
-            </div>
-            {/* TITLE */}
-            {/* RATING */}
-            <div className="flex items-end">
-              <div className="ratings mt-2 flex items-center">
-                <p>*****</p>
-                <p className="text-reg ml-3">4.9</p>
-              </div>
-              <div>
-                <p className="text-reg ml-4">600 Reviews</p>
-              </div>
-            </div>
-            {/* RATING */}
-            {/* DESCRIPTION */}
-            <div className="mt-4">
-              <p className="text-lg font-light">
-                The classics you love prepared with a perfect twist, all served
-                up in an atmosphere that feels just right. That’s the Milestones
-                promise. So, whether you’re celebrating a milestone, making the
-                most of Happy Hour or enjoying brunch with friends, you can be
-                sure that every Milestones experience is a simple and perfectly
-                memorable one.
-              </p>
-            </div>
-            {/* DESCRIPTION */}
+            <RestaurantTitle />
+            <RestaurantRating />
+            <RestaurantDescription />
             <RestaurantImages />
             <RestaurantReviews />
           </div>
